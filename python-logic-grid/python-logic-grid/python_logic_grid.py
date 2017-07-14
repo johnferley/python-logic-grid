14d660d78144a131aa57d0fb33806a36525de78e
johnferley <jferley@googlemail.com>
2017-07-14 19:38:31 +0100

    * Bug fixes * Ouput formatting improvements * Minor code tidying

--- python-logic-grid/python-logic-grid/python_logic_grid.py
+++ python-logic-grid/python-logic-grid/python_logic_grid.py
@@ -31,7 +31,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p1_cell[0],",",p1_cell[1],"] =", self.grid.values[p1_cell[0]][p1_cell[1]], " removed",str(self))
                         self.grid.values[p1_cell[0]][p1_cell[1]] = ""
                 for p2_cell in p2_cells:
                     ok = False
@@ -40,7 +39,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p2_cell[0],",",p2_cell[1],"] =", self.grid.values[p2_cell[0]][p2_cell[1]], " removed",str(self))
                         self.grid.values[p2_cell[0]][p2_cell[1]] = ""
             elif self.r == "Right":
                 for p1_cell in p1_cells:
@@ -50,7 +48,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p1_cell[0],",",p1_cell[1],"] =", self.grid.values[p1_cell[0]][p1_cell[1]], " removed",str(self))
                         self.grid.values[p1_cell[0]][p1_cell[1]] = ""
                 for p2_cell in p2_cells:
                     ok = False
@@ -59,7 +56,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p2_cell[0],",",p2_cell[1],"] =", self.grid.values[p2_cell[0]][p2_cell[1]], " removed",str(self))
                         self.grid.values[p2_cell[0]][p2_cell[1]] = ""
             elif self.r == "Has":
                 for p1_cell in p1_cells:
@@ -69,7 +65,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p1_cell[0],",",p1_cell[1],"] =", self.grid.values[p1_cell[0]][p1_cell[1]], " removed",str(self))
                         self.grid.values[p1_cell[0]][p1_cell[1]] = ""
                 for p2_cell in p2_cells:
                     ok = False
@@ -78,7 +73,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p2_cell[0],",",p2_cell[1],"] =", self.grid.values[p2_cell[0]][p2_cell[1]], " removed",str(self))
                         self.grid.values[p2_cell[0]][p2_cell[1]] = ""
             elif self.r == "Neighbor":
                 for p1_cell in p1_cells:
@@ -88,7 +82,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p1_cell[0],",",p1_cell[1],"] =", self.grid.values[p1_cell[0]][p1_cell[1]], " removed",str(self))
                         self.grid.values[p1_cell[0]][p1_cell[1]] = ""
                 for p2_cell in p2_cells:
                     ok = False
@@ -97,7 +90,6 @@ class Rule():
                             ok = True
                             break
                     if not ok:
-                        print("[",p2_cell[0],",",p2_cell[1],"] =", self.grid.values[p2_cell[0]][p2_cell[1]], " removed",str(self))
                         self.grid.values[p2_cell[0]][p2_cell[1]] = ""
         else:
             for row in range(0, len(self.grid.values)):
@@ -109,10 +101,8 @@ class Rule():
                             ok = True
                     if ok:
                         if self.grid.values[row][col] != self.p1 and str(col + 1) == self.p2:
-                            print("[",row,",",col,"] =", self.grid.values[row][col], " removed",str(self))
                             self.grid.values[row][col] = ""
                         elif self.grid.values[row][col] == self.p1 and str(col + 1) != self.p2:
-                            print("[",row,",",col,"] =", self.grid.values[row][col], " removed",str(self))
                             self.grid.values[row][col] = ""
 
 class Grid():
@@ -124,13 +114,18 @@ class Grid():
         self.locations = ["1", "2", "3", "4", "5"]
         self.no_of_properties = len(self.properties)
         self.no_of_combinations = len(self.properties[0])
+        self.output_text = "The {4} lives in house number {0}, which is painted {1}. He keep {2}. drinks {3}, and smokes {5} cigarettes."
         for property in self.properties:
             for each in property:
-                temp = [each, each, each, each, each]
+                temp = [each for x in range(0, self.no_of_properties)]
                 self.values.append(temp)
     def __str__(self):
         l = 0
         output = ""
+        for rule in self.rules:
+            output += str(rule)
+            output += "\n"
+        output += "\n"
         for row in self.values:
             for col in row:
                 if len(col) > l:
@@ -162,6 +157,16 @@ class Grid():
                     output += "-" * l
                 output += "+"
                 output += "\n"
+        output += "\n"
+        for house in range(0, len(self.locations)):
+            temp = [None for x in range(0, self.no_of_properties)]
+            for i in range(0, self.no_of_properties):
+                s = ""
+                for row in range(0 + (i * self.no_of_combinations), self.no_of_combinations + (i * self.no_of_combinations)):
+                    s += self.values[row][house]
+                temp[i] = s
+            output += self.output_text.format(self.locations[house], temp[0], temp[1], temp[2], temp[3], temp[4])
+            output += "\n"
         return output.rstrip()
     def add_rule(self, p1, r, p2, text):
         if r == "At":
@@ -192,24 +197,6 @@ class Grid():
         for rule in self.rules:
             rule.validate()
         counter = 0
-        #for row in range(0, len(self.values)):
-        #    if self.values[row].count("") == len(self.values[row]) - 1:
-        #        for property in self.values[row]:
-        #            if property != "":
-        #                p = property
-        #                i = self.values[row].index(p)
-        #                break
-        #        for row2 in range(0, len(self.values)):
-        #            for col in range(0, self.no_of_properties):
-        #                if col == i:
-        #                    if self.values[row2][col] != "" and self.values[row2][col] != p:
-        #                        ok = False
-        #                        for property in self.properties:
-        #                            if self.values[row2][col] in property and p in property:
-        #                                ok = True
-        #                        if ok:
-        #                            counter += 1
-        #                            self.values[row2][col] = ""
         for prop in range(0, self.no_of_properties):
             for row in range(0 + (prop * self.no_of_combinations), self.no_of_combinations + (prop * self.no_of_combinations)):
                 if self.values[row].count("") == len(self.values[row]) - 1:
@@ -220,7 +207,6 @@ class Grid():
                             break
                     for row2 in range(0 + (prop * self.no_of_combinations), self.no_of_combinations + (prop * self.no_of_combinations)):
                         if self.values[row2][j] != "" and row2 != i:
-                            print("[",row2,",",j,"] =", self.values[row2][j], " removed")
                             counter += 1
                             self.values[row2][j] = ""
             for col in range(0, self.no_of_properties):
@@ -230,12 +216,9 @@ class Grid():
                         n += 1
                         i = row
                         j = col
-                        if n == 1:
-                            break
                 if n == 1:
                     for col2 in range(0, self.no_of_properties):
                         if col2 != j and self.values[i][col2] != "":
-                            print("[",i,",",col2,"] =", self.values[i][col2], " removed")
                             counter += 1
                             self.values[i][col2] = ""
 
@@ -251,7 +234,7 @@ class Main():
         puzzle.add_rule("Green", "Left", "White", "The {0} house is on the {1} of the {2} house")
         puzzle.add_rule("Green", "Has", "Coffee", "The {0} homeowner drinks {2}")
         puzzle.add_rule("Pall Mall", "Has", "Birds", "The person who smokes {0} rears {2}")
-        puzzle.add_rule("Yellow", "Has", "Dunhill", "The owber if the {0} house smokes {2}")
+        puzzle.add_rule("Yellow", "Has", "Dunhill", "The owner of the {0} house smokes {2}")
         puzzle.add_rule("Milk", "At", "3", "The man living in the center house drinks {0}")
         puzzle.add_rule("Norwegian", "At", "1", "The {0} lives in the first house")
         puzzle.add_rule("Blend", "Neighbor", "Cats", "The man who smokes {0} lives next to the one who keeps {2}")
